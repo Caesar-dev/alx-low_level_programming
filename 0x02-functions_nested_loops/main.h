8dcc9f8ef0db7749ba189b345d6719fe0bb099c0
McCaesar-dev <aflameyb@gmail.com>
2021-09-01 16:44:22 +0000

    wert

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,12 +1,14 @@
 #include "main.h"
 
 /**
- * main -check the code for ALX School students.
+ *main - check the code for ALX School students.
  *
- * Return: Always 0.
+ *Return: Always 0.
  */
 int main(void)
 {
+
   print_alphabet();
   return (0);
+
 }
